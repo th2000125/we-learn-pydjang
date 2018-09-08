cff62763fbdbcf5894845f94bb92448544d612a2
Unknown <thomas.hussey@zaoby.co.uk>
2018-09-08 13:05:42 +0100

    Commit to master - Changes to show jack how import works
    Shows how you can use "Import time" once and all the sleep will still work

--- Test.py
+++ Test.py
@@ -1,3 +1,12 @@
-Hello Tom
+import time
 
 print("Bite, me.")
+#hello Jack. I fixed your code.
+
+time.sleep(4)
+print("Bite, me. 2nd")
+time.sleep(1)
+print("Bite, me. 3rd")
+time.sleep(1)
+print("Bite, me. 4th")
+time.sleep(5)
