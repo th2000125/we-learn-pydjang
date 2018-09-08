747a7b65a334def6682892185d0b7c2721ed6bba
Unknown <halfzebra.zaoby@gmail.com>
2018-09-08 13:15:03 +0100

    while true
    loop

--- Test.py
+++ Test.py
@@ -1,12 +1,13 @@
-import time
+while True:
+	import time
 
-print("Bite, me.")
-#hello Jack. I fixed your code.
+	print("Bite, me.")
+	#hello Jack. I fixed your code.
 
-time.sleep(4)
-print("Bite, me. 2nd")
-time.sleep(1)
-print("Bite, me. 3rd")
-time.sleep(1)
-print("Bite, me. 4th")
-time.sleep(5)
+	time.sleep(4)
+	print("Bite, me. 2nd")
+	time.sleep(1)
+	print("Bite, me. 3rd")
+	time.sleep(1)
+	print("Bite, me. 4th")
+	time.sleep(5)
